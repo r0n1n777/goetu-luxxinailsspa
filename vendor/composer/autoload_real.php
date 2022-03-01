6462f90cf2c323bfe3ea26fdb9527f4ee946fb1d
Ero Aguilar <eroaguilar7@gmail.com>
2022-03-01 23:19:23 +0800

    Added vendor folder.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit73316465dfcbc4080a161ef0e3108fbe
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit73316465dfcbc4080a161ef0e3108fbe', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit73316465dfcbc4080a161ef0e3108fbe', 'loadClassLoader'));
